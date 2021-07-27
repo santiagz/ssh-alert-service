899887ed9771fc35dbb0c63b9e34e7a71a6024e4
santiagz <72973604+santiagz@users.noreply.github.com>
2021-07-27 16:20:43 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,3 +1,3 @@
-chat_id = "368638207"                                       #EDIT YOUR CHATID
-BOT_API = "1890399743:AAHta8_ThoV92QrJIs2PmEf7_VQpXj27_2A"  #EDIT YOUR BOT API
+chat_id = "111111111"                                       #EDIT YOUR CHATID
+BOT_API = "11111111:mhulh1mlh11ljh1lmh"  #EDIT YOUR BOT API
 car_name = "test_tpot_1"                                    #EDIT FOR YOUR VM/SERVER NAME/IP WHATEVER
